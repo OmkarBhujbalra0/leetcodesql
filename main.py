48182108ff5dc7cf7297e77bf239d18d8c42e6f5
Omkar Bhujbalrao <141210230+OmkarBhujbalra0@users.noreply.github.com>
2025-10-08 18:30:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Big Countries
-
-def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    world = world[(world['area'] >= 3000000) | (world['population']>= 25000000)]
-    return world[['name','population','area']]
